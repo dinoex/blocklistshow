16c7bbf74b595093297e65fc0c9955be0ddb1607
cvs <cvs>
2020-05-08 12:02:07 +0000

    Initial revision

---
+++ bin/blocklist.rb
@@ -0,0 +1,75 @@
+#!/usr/local/bin/ruby -w
+
+require 'ipaddr'
+require 'json'
+require 'pp'
+
+DNS_CACHE_FILE = '/var/db/blacklistd.dns.json'
+CC_CACHE_FILE = '/var/db/blacklistd.cc.json'
+
+def get_dns( ip )
+  return @dns_cache[ ip ] if @dns_cache.key?( ip )
+  `host '#{ip}'`.split( "\n" ).each do |line|
+    return 'not found' if line =~ /not found/
+    return line.split( /[\s]/ ).last if line =~ /domain name pointer/
+    return line
+  end
+  nil
+end
+
+def get_cached_dns( ip )
+  return @dns_cache[ ip ] if @dns_cache.key?( ip )
+  @dns_cache[ ip ] = get_dns( ip )
+end
+
+def get_cc( ip )
+  `whois '#{ip}'`.force_encoding( 'BINARY' ).split( "\n" ).each do |line|
+    # pp line
+    case line
+    when /^country:/i
+      return line.split( ':', 2 ).last.strip
+    end
+  end
+  exit 1
+end
+
+def get_cached_cc( ip )
+  return @cc_cache[ ip ] if @cc_cache.key?( ip )
+  @cc_cache[ ip ] = get_cc( ip )
+end
+
+def load_json( filename )
+  result = {}
+  if File.exist?( filename )
+    raw = File.read( filename )
+    result = JSON.parse( raw )
+  end
+  result
+end
+
+def load_cache
+  @dns_cache = load_json( DNS_CACHE_FILE )
+  @cc_cache = load_json( CC_CACHE_FILE )
+end
+
+def save_cache
+  File.write( DNS_CACHE_FILE, JSON.dump( @dns_cache ) + "\n" )
+  File.write( CC_CACHE_FILE, JSON.dump( @cc_cache ) + "\n" )
+end
+
+load_cache
+raw = `blacklistctl dump -b`
+# pp raw
+raw.split( "\n" ) do |line|
+  address_port, _state, _nfail, access = line.split( "\t", 4 )
+  next if access == 'last access'
+  # p [ address_port, access ]
+  ip = address_port.split( '/' ).first.strip
+  cc = get_cached_cc( ip )
+  dns = get_cached_dns( ip )
+  puts "#{address_port}\t#{access}\t#{cc}\t#{dns}"
+end
+save_cache
+
+exit 0
+# eof
