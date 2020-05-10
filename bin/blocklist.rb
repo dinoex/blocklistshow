6da2e5f6bc9bdbd544730022ca92a22e3515fae1
cvs <cvs>
2020-05-10 07:30:11 +0000

    - force_encoding - drop header - sort by date - add whitelist - rubocop

--- bin/blocklist.rb
+++ bin/blocklist.rb
@@ -4,14 +4,16 @@ require 'ipaddr'
 require 'json'
 require 'pp'
 
-DNS_CACHE_FILE = '/var/db/blacklistd.dns.json'
-CC_CACHE_FILE = '/var/db/blacklistd.cc.json'
+DNS_CACHE_FILE = '/var/db/blacklistd.dns.json'.freeze
+CC_CACHE_FILE = '/var/db/blacklistd.cc.json'.freeze
 
 def get_dns( ip )
   return @dns_cache[ ip ] if @dns_cache.key?( ip )
+
   `host '#{ip}'`.split( "\n" ).each do |line|
     return 'not found' if line =~ /not found/
     return line.split( /[\s]/ ).last if line =~ /domain name pointer/
+
     return line
   end
   nil
@@ -19,11 +21,12 @@ end
 
 def get_cached_dns( ip )
   return @dns_cache[ ip ] if @dns_cache.key?( ip )
+
   @dns_cache[ ip ] = get_dns( ip )
 end
 
 def get_cc( ip )
-  `whois '#{ip}'`.force_encoding( 'BINARY' ).split( "\n" ).each do |line|
+  `whois '#{ip}'`.split( "\n" ).each do |line|
     # pp line
     case line
     when /^country:/i
@@ -35,6 +38,7 @@ end
 
 def get_cached_cc( ip )
   return @cc_cache[ ip ] if @cc_cache.key?( ip )
+
   @cc_cache[ ip ] = get_cc( ip )
 end
 
@@ -58,16 +62,27 @@ def save_cache
 end
 
 load_cache
-raw = `blacklistctl dump -b`
+list = []
+raw = `blacklistctl dump -b -n`
 # pp raw
-raw.split( "\n" ) do |line|
-  address_port, _state, _nfail, access = line.split( "\t", 4 )
-  next if access == 'last access'
+raw.split( "\n" ).each do |line|
+  address_port, state, _nfail, access = line.split( "\t", 4 )
+  list.push( [ access, address_port, state ] )
+end
+list.sort.each do |row|
+  access, address_port, state = row
   # p [ address_port, access ]
   ip = address_port.split( '/' ).first.strip
   cc = get_cached_cc( ip )
   dns = get_cached_dns( ip )
-  puts "#{address_port}\t#{access}\t#{cc}\t#{dns}"
+  white =
+    case state
+    when 'OK'
+      ''
+    else
+      'OK/'
+    end
+  puts "#{address_port}\t#{access}\t#{white}#{cc}\t#{dns}"
 end
 save_cache
 
