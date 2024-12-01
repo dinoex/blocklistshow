d0193ad17001db0bf1db6510ddc311ea3258ba0a
cvs <cvs>
2024-12-01 16:28:00 +0000

    - support new filenames - call helper to unblock

--- bin/unblock.rb
+++ bin/unblock.rb
@@ -1,6 +1,29 @@
 #!/usr/local/bin/ruby
 
-BDB_FILE = '/var/db/blacklistd.db'.freeze
+def find_files( list )
+  list.each do |filename|
+    next if filename == ''
+
+    return filename if File.exist?( filename )
+  end
+
+  list.first
+end
+
+BDB_FILES = [
+  '/var/db/blocklistd.db',
+  '/var/db/blacklistd.db'
+].freeze
+
+HELPER_FILES = [
+  '/usr/local/libexec/blocklistd-helper',
+  '/usr/libexec/blacklistd-helper',
+  '/libexec/blocklistd-helper'
+].freeze
+
+BDB_FILE = find_files( BDB_FILES ).freeze
+HELPER_FILE = find_files( HELPER_FILES ).freeze
+
 LOCAL_BDB_FILE_MODE = 0o0600
 LOCAL_BDB_OPTIONS = { 'set_pagesize' => 1024, 'set_cachesize' => 32 * 1024 }.freeze
 
@@ -78,15 +101,61 @@ def search_db( dbh, list )
   found
 end
 
+PROTOCOLS = {
+  6 => 'tcp',
+  17 => 'udp',
+  132 => 'sctp'
+}.freeze
+
+def decode_key( key )
+  af = key[ 1 .. 1 ].unpack1( 'C' )
+  {
+    af: af,
+    ip: decode_ip( key, af ),
+    mask: key[ 128 .. 131 ].unpack1( 'L' ),
+    port: key[ 132 .. 135 ].unpack1( 'L' ),
+    proto: PROTOCOLS[ key[ 136 .. 139 ].unpack1( 'L' ) ],
+    family: key[ 140 .. 143 ].unpack1( 'L' ),
+    uid: key[ 144 .. 147 ].unpack1( 'L' ),
+    nfail: key[ 148 .. 151 ].unpack1( 'L' ),
+    name: key[ 152 .. 279 ].delete( "\0" ),
+    rmask: key[ 280 .. 283 ].unpack1( 'L' ),
+    duration: key[ 284 .. 287 ].unpack1( 'L' )
+  }
+end 
+
+def decode_data( key )
+  {
+    count: key[ 0 .. 3 ].unpack1( 'L' ),
+    pad1: key[ 4 .. 7 ].unpack1( 'L' ),
+    time1: Time.at( key[ 8 .. 11 ].unpack1( 'L' ) ),
+    time2: key[ 12 .. 15 ].unpack1( 'L' ),
+    text: key[ 16 .. 79 ].delete( "\0" )
+  }
+end 
+
 def remove_db( dbh, found )
   removed = 0
   found.each do |key|
+    # pp key
+    h = decode_key( key )
+    # data = dbh[ key ]
+    # pp decode_data( data )
     dbh.delete( key )
+    # pp h
+    line = "#{HELPER_FILE} 'rem' '#{h[ :name ]}' '#{h[ :proto ]}' '#{h[ :ip ]}' '#{h[ :mask ]}' '#{h[ :port ]}' '#{h[ :uid ]}'"
+    puts line
+    `#{line}`
     removed += 1
   end
   puts "removed: #{removed}"
   dbh.sync
   dbh.close
+end
+
+if ARGV.empty?
+  warn "#{$0} IP-Addresss [ IP-Addresss ] [ ... ]"
+  exit 64
 end
 
 dbh = BDB1::Hash.open( BDB_FILE,
